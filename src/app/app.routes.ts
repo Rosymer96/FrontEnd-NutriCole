8e9fd60a9fd77f8768e930b5bc15321fb5c34914
Rosa Vela <rosymer96@gmail.com>
2025-06-29 14:18:50 +0200

    componentes anadidos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { NavbarComponentComponent } from './components/navbar-component/navbar-component.component';
+import { FooterComponentComponent } from './components/footer-component/footer-component.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  {path: 'header',component: NavbarComponentComponent},
+  {path: 'footer',component:FooterComponentComponent}
 ];
 
 //Ejemplo de paths usando la verificacion de las guards:
