09514253121183a585d6ea440dff62995bbbd579
Rosa Vela <rosymer96@gmail.com>
2025-07-02 23:30:55 +0200

    Header y footer anadidos, el boton de logout solo aparece cuando hacemos login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponentComponent } from './components/navbar-component/navbar-component.component';
+import { FooterComponentComponent } from './components/footer-component/footer-component.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponentComponent, FooterComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/navbar-component/navbar-component.component.ts
+++ src/app/components/navbar-component/navbar-component.component.ts
@@ -9,14 +9,17 @@ import { AuthService } from '../../services/auth/auth.service';
   styleUrl: './navbar-component.component.css',
 })
 export class NavbarComponentComponent {
-  goTo(route: string) {}
   private router = inject(Router);
   private auth = inject(AuthService);
+
+  isLoggedIn(): boolean {
+    return this.auth.isLoggedIn();
+  }
 
   logout() {
     // Lógica de cierre de sesión: ajusta según tu AuthService
     this.auth.logout();
     // Redirige a login o a la home
-    this.router.navigateByUrl('/login');
+    this.router.navigateByUrl('/user/login');
   }
 }
